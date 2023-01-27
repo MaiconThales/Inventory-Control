097bbfff2f8587237b2929352224309b8219f869
Maicon Thales <maiconthales34@hotmail.com>
2023-01-27 08:13:26 -0300

    Start project

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: "https://jwthxonnohkfgagcesku.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3dGh4b25ub2hrZmdhZ2Nlc2t1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ4MTYxODksImV4cCI6MTk5MDM5MjE4OX0.n17jmakJXtFVNlphMQ8_U2B3UJMr8CJnPBQLRM-kAeE"
+};
